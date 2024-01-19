aed5076096f3bb954dbcc9cec3b2c59759af5b95
Liel Almog <liel.almogg@gmail.com>
2024-01-19 14:29:18 +0000

    Create a storage-coordinator service that is responsible for handling file uploads to the permanent and backup storages

---
+++ storage-coordinator/configs/env.go
@@ -0,0 +1,59 @@
+package configs
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	initEnvOnce sync.Once
+
+	envs map[string]*string
+)
+
+func InitEnv() {
+	initEnvOnce.Do(func() {
+		envs = make(map[string]*string)
+		env, err := godotenv.Read()
+		if err != nil {
+			panic(err)
+		}
+
+		for key, value := range env {
+			env := value
+			envs[key] = &env
+		}
+	})
+}
+
+func GetAllEnvs() (map[string]string, error) {
+	if envs == nil {
+		return nil, fmt.Errorf("envs is nil")
+	}
+
+	envMap := make(map[string]string)
+	for key, value := range envs {
+
+		// Remeber that go reuse the value variable in the loop
+		// https://medium.com/swlh/use-pointer-of-for-range-loop-variable-in-go-3d3481f7ffc9
+		env := *value
+		envMap[key] = env
+	}
+
+	return envMap, nil
+}
+
+func GetEnv(key string) (string, error) {
+	if envs == nil {
+		return "", fmt.Errorf("envs is nil")
+	}
+
+	value, ok := envs[key]
+	if !ok {
+		return "", fmt.Errorf("key %s not found", key)
+	}
+
+	return *value, nil
+}
--- storage-coordinator/main.go
+++ storage-coordinator/main.go
@@ -1,11 +1,41 @@
 package main
 
+import (
+	"context"
+	"strconv"
+	"strings"
+
+	"github.com/lielalmog/file-uploader/storage-coordinator/configs"
+	"github.com/segmentio/kafka-go"
+)
+
 func main() {
+	configs.InitEnv()
+	const topic = "file-upload-finalization"
+
+	brokers, err := configs.GetEnv("KAFKA_BROKERS")
+	if err != nil {
+		panic(err)
+	}
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: strings.Split(brokers, ","),
+		Topic:   topic,
+		GroupID: "upload-permanent-backup",
+	})
 
 	for {
-		// listen for event
+		m, err := r.FetchMessage(context.Background())
+		if err != nil {
+			break
+		}
 
-		// To backup -> DB update
-		// To permanent -> DB update
+		// bytes to int64
+		id, err := strconv.ParseInt(string(m.Value), 0, 64)
+		if err != nil {
+			continue
+		}
+
+		go combineChunksAndUploadToPermanent(id)
 	}
 }
