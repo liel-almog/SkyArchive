9ea0184ccafb1e0f2130adbb6c262c25a020403c
Liel Almog <liel.almogg@gmail.com>
2024-01-22 14:36:52 +0200

    Fix: Use InputPassword on password instead of email

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,9 +1,6 @@
 import { FormProvider } from "react-hook-form";
 import { Link } from "react-router-dom";
-import {
-  Input,
-  InputPassword,
-} from "../../components/common/FormElements/Input";
+import { Input, InputPassword } from "../../components/common/FormElements/Input";
 import classes from "./login.module.scss";
 import { useLogin } from "./useLogin";
 
@@ -18,16 +15,9 @@ export const LoginForm = () => {
           <p>אנא הכנס את האימייל והסיסמא על מנת להתחבר לחשבון</p>
         </header>
         <FormProvider {...methods}>
-          <form
-            className={classes.form}
-            onSubmit={methods.handleSubmit(onSubmit)}
-          >
-            <InputPassword
-              name="email"
-              placeholder="m@example.com"
-              label="אימייל"
-            />
-            <Input name="password" label="סיסמא" type="password" />
+          <form className={classes.form} onSubmit={methods.handleSubmit(onSubmit)}>
+            <Input name="email" placeholder="m@example.com" label="אימייל" />
+            <InputPassword name="password" label="סיסמא" type="password" />
             <button type="submit">שליחה</button>
             <p>
               אין חשבון עדיין? <Link to="/signup">הרשמה</Link>
