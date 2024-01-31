bededde72a6e17d75bc31e191b897ebb4f70b17c
Liel Almog <liel.almogg@gmail.com>
2024-01-31 19:20:39 +0000

    Feat: SAS Token uploads to Azure Blob Storage. Before we upload a file we send a request to the backend with the file metadata. The backend returns a SAS token and an ID. The frontend uploads the file to the storage service directly with the SAS Token.

--- backend/services/upload_service.go
+++ backend/services/upload_service.go
@@ -2,12 +2,14 @@ package services
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"mime/multipart"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/sas"
 	"github.com/lielalmog/file-uploader/backend/configs"
 	"github.com/lielalmog/file-uploader/backend/kafka"
 	"github.com/lielalmog/file-uploader/backend/models"
@@ -16,8 +18,8 @@ import (
 )
 
 type UploadService interface {
-	StartUpload(ctx context.Context, fileMetadate *models.UploadFileMetadateDTO) (*int64, error)
-	UploadChunk(ctx context.Context, fileHeader *multipart.FileHeader, id int64, chunkIndex int) error
+	SaveFileMetadata(ctx context.Context, fileMetadate *models.UploadFileMetadateDTO) (*int64, error)
+	GenerateSasToken(ctx context.Context, fileId *int64) (*string, error)
 	CompleteUploadEvent(ctx context.Context, id int64) error
 }
 
@@ -35,32 +37,66 @@ const (
 	permanentContainerName = "permanent-files"
 )
 
-func (u *uploadServiceImpl) StartUpload(ctx context.Context, fileMetadate *models.UploadFileMetadateDTO) (*int64, error) {
+func parseAzureStorageConnectionString(connectionString string) (accountName, accountKey string, err error) {
+	parts := strings.Split(connectionString, ";")
+	for _, part := range parts {
+		if strings.HasPrefix(part, "AccountName=") {
+			accountName = strings.TrimPrefix(part, "AccountName=")
+		} else if strings.HasPrefix(part, "AccountKey=") {
+			accountKey = strings.TrimPrefix(part, "AccountKey=")
+		}
+	}
+
+	if accountName == "" || accountKey == "" {
+		return "", "", errors.New("account name or key not found in connection string")
+	}
+
+	return accountName, accountKey, nil
+}
+
+func (u *uploadServiceImpl) SaveFileMetadata(ctx context.Context, fileMetadate *models.UploadFileMetadateDTO) (*int64, error) {
 	return u.uploadRepository.SaveFileMetadata(ctx, fileMetadate)
 }
 
-func (u *uploadServiceImpl) UploadChunk(ctx context.Context, fileHeader *multipart.FileHeader, id int64, chunkIndex int) error {
+func (u *uploadServiceImpl) GenerateSasToken(ctx context.Context, fileId *int64) (*string, error) {
 	connectionString, err := configs.GetEnv("AZURE_STORAGE_CONNECTION_STRING")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	file, err := fileHeader.Open()
+	accountName, accountKey, err := parseAzureStorageConnectionString(connectionString)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	uploadCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	serviceClient, err := azblob.NewClientFromConnectionString(connectionString, nil)
-	blobName := fmt.Sprintf("%d/%d", id, chunkIndex)
-	serviceClient.UploadStream(uploadCtx, tempContainerName, blobName, file, nil)
+	// extract the account name and key from the connection string
+	credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	// create a SAS token that's valid for one hour.
+	// since this is a network call, there's a timeout.
+	sasPermissions := sas.BlobPermissions{Create: true, Add: true, Write: true, Read: true, Delete: true, List: true}
+	fileName := fmt.Sprintf("%d", *fileId)
+
+	token, err := sas.BlobSignatureValues{
+		Protocol:    sas.ProtocolHTTPS,
+		ExpiryTime:  time.Now().Add(1 * time.Hour),
+		Permissions: sasPermissions.String(),
+
+		// Start time 10 minutes ago to avoid clock skew.
+		StartTime:     time.Now().Add(-10 * time.Minute),
+		ContainerName: tempContainerName,
+		BlobName:      fileName,
+	}.SignWithSharedKey(credential)
+	if err != nil {
+		return nil, err
+	}
+
+	signedUrl := fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s?%s", accountName, tempContainerName, fileName, token.Encode())
+
+	return &signedUrl, nil
 }
 
 func (u *uploadServiceImpl) CompleteUploadEvent(ctx context.Context, id int64) error {
--- frontend/src/services/upload.service.ts
+++ frontend/src/services/upload.service.ts
@@ -1,3 +1,4 @@
+import { BlobClient } from "@azure/storage-blob";
 import { startUploadSchema } from "../models/upload.model";
 import { authenticatedInstance } from "./index.service";
 
@@ -5,46 +6,27 @@ const PREFIX = "upload" as const;
 
 export class UploadService {
   async uploadFile(file: File) {
-    const { id, CHUNKS_COUNT, CHUNK_SIZE } = await this.startUpload(file);
+    const { id, signedUrl } = await this.startUpload(file);
 
-    const upload = (chunkIndex: number) => {
-      const formData = new FormData();
-      formData.append(
-        "file",
-        file.slice(chunkIndex * CHUNK_SIZE, chunkIndex * CHUNK_SIZE + CHUNK_SIZE)
-      );
-      formData.append("index", chunkIndex.toString());
-      formData.append("total", CHUNKS_COUNT.toString());
-      return authenticatedInstance.post(`/${PREFIX}/chunk/${id}/${chunkIndex}`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-    };
-
-    for (let chunkIndex = 0; chunkIndex < CHUNKS_COUNT; chunkIndex++) {
-      await upload(chunkIndex);
-    }
+    const blobClient = new BlobClient(signedUrl);
+    const blockBlobClient = blobClient.getBlockBlobClient();
+    await blockBlobClient.uploadData(file);
 
     await this.completeUpload(id);
   }
 
   private async startUpload(file: File) {
-    const CHUNK_SIZE = 1024 * 1024; // 1MB
     const SIZE = file.size;
-    const CHUNKS_COUNT = Math.ceil(SIZE / CHUNK_SIZE);
-    const { data } = await authenticatedInstance.post(`/${PREFIX}/chunk/start`, {
+    const { data } = await authenticatedInstance.post(`/${PREFIX}/start`, {
       fileName: file.name,
       size: SIZE,
     });
 
-    const { id } = startUploadSchema.parse(data);
-
-    return { id, CHUNK_SIZE, CHUNKS_COUNT };
+    return startUploadSchema.parse(data);
   }
 
   private async completeUpload(id: number) {
-    await authenticatedInstance.post(`/${PREFIX}/chunk/complete/${id}`);
+    await authenticatedInstance.post(`/${PREFIX}/complete/${id}`);
   }
 }
 
